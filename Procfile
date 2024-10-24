15e8c9162ba845ea9053509c6cbb728ad88584ff
mehmetfaahem <mehmetfaahem@gmail.com>
2024-10-24 19:50:19 +0600

    ready to production

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn my_admin_panel.wsgi --log-file -
--- my_admin_panel/wsgi.py
+++ my_admin_panel/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_admin_panel.settings')
 
 application = get_wsgi_application()
+
